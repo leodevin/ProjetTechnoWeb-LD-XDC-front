a1ab23a2812cb4dd599f2781b78e5204088dc5fb
Xavier <xavier.decazenove@outlook.fr>
2019-12-10 01:00:01 +0100

    Create fonctionnel + MAJ aussi

--- src/components/Main-Card-Form.jsx
+++ src/components/Main-Card-Form.jsx
@@ -1,48 +1,140 @@
 import React, {Component} from "react";
 import {Card, Button, Row, Col, Form} from "react-bootstrap";
-import '../css/Main-Form.css'
-import axios from 'axios'
+import '../css/Main-Form.css';
+import axios from 'axios';
+
+// MODELES DES FORMS
+const creatUserChamps = [
+    {nomChamps: "ID", nomPlaceHolder: "ObjectID", name: "id", type: "text"},
+    {nomChamps: "LOCATION", nomPlaceHolder: "Pays", name: "location", type: "text"},
+    {nomChamps: "PERSONNES DANS LA MAISON", nomPlaceHolder: "Nombre", name: "personnes", type: "number"},
+    {nomChamps: "TAILLE DE LA MAISON", nomPlaceHolder: "Big/Medium/Small", name: "taille", type: "text"}];
+
+const deleteUserChamps = [
+    {nomChamps: "ID", nomPlaceHolder: "Id de l'utilisateur à supprimer", name: "id", type: "text"}];
 
 class Main_Card_Form extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedChamps: [
-                {nomChamps: "ID", nomPlaceHolder: "ObjectID"},
-                {nomChamps: "LOCATION", nomPlaceHolder: "Pays"},
-                {nomChamps: "PERSONNES DANS LA MAISON", nomPlaceHolder: "Nombre"},
-                {nomChamps: "TAILLE DE LA MAISON", nomPlaceHolder: "Big/Medium/Small"}
-            ],
-            id: "",
-            location: "",
-            personnes: "",
-            taille: ""
+            champs : creatUserChamps,
+            valueChampsSelected: 1
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.creatUser = this.creatUser.bind(this);
+        this.updateUser = this.updateUser.bind(this);
+        this.deleteUser = this.deleteUser.bind(this);
     }
 
     handleChange(event) {
         this.setState({[event.target.name]: event.target.value});
     }
 
-    handleSubmit(event) {
+    // Requet pour le form : creation d'utilisateur
+    creatUser(event){
         axios.post('http://localhost:3000/user', {
-            _id: this.state.id,
+            userId: this.state.id,
             location: this.state.location,
             personsInHouse: parseInt(this.state.personnes),
             houseSize: this.state.taille
         })
             .then(function (response) {
-                console.log(response);
                 event.preventDefault();
+                console.log(response);
             })
             .catch(function (error) {
                 console.log(error);
             });
     }
 
+    updateUser(event){
+        axios.put('http://localhost:3000/user/'+this.state.id, {
+            userId: this.state.id,
+            location: this.state.location,
+            personsInHouse: parseInt(this.state.personnes),
+            houseSize: this.state.taille
+        })
+            .then(function (response) {
+                event.preventDefault();
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    //event.preventDefault();
+
+    //Requet pour le form : delete d'utilisateur
+    deleteUser(event){
+        axios.delete('http://localhost:3000/user/' + this.state.id)
+            .then(function (response) {
+                alert("Utilisateur bien supprimer");
+                console.log(response);
+                axios.get('http://localhost:3000/user/'+ this.state.id +'/sensors')
+                    .then(function (sensors) {
+                        for (let i=0; i<sensors.length; i++){
+                            axios.delete('http://localhost:3000/sensor/'+sensors[i]._id)
+                                .then(function (response) {
+                                    axios.get('http://localhost:3000/measure/'+sensors[i]._id+'/measures')
+                                        .then(function (measures) {
+                                            for (let i=0; i<measures.length; i++){
+                                                axios.delete('http://localhost:3000/measure/'+measures[i]._id)
+                                                    .then(function (response2) {
+                                                        event.preventDefault();
+                                                    })
+                                            }
+                                        })
+                                })
+                        }
+                    })
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    // Centre de redirection en fonction du type de form selectionner
+    handleSubmit(event) {
+        if (this.state.valueChampsSelected == 1){
+            this.creatUser(event);
+        }
+        if (this.state.valueChampsSelected == 2){
+            this.updateUser(event);
+        }
+        if (this.state.valueChampsSelected == 3){
+            this.deleteUser(event);
+        }
+    }
+
+    // Change les champs en fonction du type selectionner
+    changeSelectButton(e){
+        if(e.target.value == 1){
+            this.setState({champs: creatUserChamps, valueChampsSelected: 1})
+        }
+        if(e.target.value == 2){
+            this.setState({champs: creatUserChamps, valueChampsSelected: 2})
+        }
+        if(e.target.value == 3){
+            this.setState({champs: deleteUserChamps, valueChampsSelected: 3})
+        }
+    }
+
     render() {
+
+        const displayChamps = this.state.champs.map((post) =>
+            <div><Row>
+                    <Col lg={4}><label>{post.nomChamps}</label></Col>
+                    <Col lg={8}><input name={post.name}
+                                       className="form-control"
+                                       type={post.type}
+                                       placeholder={post.nomPlaceHolder}
+                                       onChange={this.handleChange}/></Col>
+                </Row><br/>
+            </div>
+            );
+
         return (
             <Card className="my-lg-5 my-md-4 my-sm-4 my-4">
                 <Row className="mx-3 my-3 justify-content-between">
@@ -50,36 +142,21 @@ class Main_Card_Form extends Component {
                         <h2 className="title-form">Ajout d'un utilisateur</h2>
                     </Col>
                     <Col className="col-lg-4 col-md-6 col-6" id="col-form-button">
-                        <Form.Control as="select" variant={"primary"}>
-                            <option>AJOUTER</option>
-                            <option>UPDATE</option>
+                        <Form.Control as="select"
+                                      variant={"primary"}
+                                      onChange={this.changeSelectButton.bind(this)}>
+                            <option value={1}>AJOUTER UN UTILISATEUR</option>
+                            <option value={2}>METTRE A JOUR UN UTILISATEUR</option>
+                            <option value={3}>SUPPRIMER UN UTILISATEUR</option>
                         </Form.Control>
                     </Col>
                 </Row>
                 <Card className="card-form">
                     <Row className="my-5 justify-content-center">
-                        <Col lg={6}>
+                        <Col className={"col-8"}>
                             <Form onSubmit={this.handleSubmit}>
-                                <Row>
-                                    <Col lg={4}><label>ID</label></Col>
-                                    <Col lg={8}><input name={"id"} className="form-control" type="text" placeholder={"ObjectID"} onChange={this.handleChange}/></Col>
-                                </Row>
-                                <br/>
-                                <Row>
-                                    <Col lg={4}><label>LOCATION</label></Col>
-                                    <Col lg={8}><input name={"location"} className="form-control" type="text" placeholder={"Pays"} onChange={this.handleChange}/></Col>
-                                </Row>
-                                <br/>
-                                <Row>
-                                    <Col lg={4}><label>PERSONNES DANS LA MAISON</label></Col>
-                                    <Col lg={8}><input name={"personnes"} className="form-control" type="number" placeholder={"Nombre"} onChange={this.handleChange}/></Col>
-                                </Row>
-                                <br/>
-                                <Row>
-                                    <Col lg={4}><label>TAILLE DE LA MAISON</label></Col>
-                                    <Col lg={8}><input name={"taille"} className="form-control" type="text" placeholder={"Big/Medium/Small"} onChange={this.handleChange}/></Col>
-                                </Row>
-                                <Button className="justify-content-center button-form" type={"submit"}>AJOUTER</Button>
+                                {displayChamps}
+                                <Button className="justify-content-center button-form" type={"submit"}>ENVOYER</Button>
                             </Form>
                         </Col>
                     </Row>
